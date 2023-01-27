d17f443a67c4e94b9c7bc4e4219de23fea3036fd
PROG-007 <raghavadi007@gmail.com>
2023-01-27 11:54:09 +0530

    changed my pc

--- main.py
+++
@@ -1,8 +0,0 @@
-lst=list(map(int,input().split()))
-n=int(input())
-temp=[]
-for i in range(len(lst)):
-    for j in range(len(lst)):
-        if i!=j and (lst[i]+lst[j]) == n and j not in temp:
-            print(lst[i],lst[j])
-            temp.append(i)
