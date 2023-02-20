fb8295f8371f2cd744802e1f00cba0495eb485f3
PROG-007 <raghavadi007@gmail.com>
2023-02-20 22:36:51 +0530

    some donut thing I tried

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.set_int_max_str_digits(0)
-while True:
-    a=int(input("Enter the number, whose factorial you want: "))
-    b=1
-    for i in range(a,0,-1):
-        b*=i
-    print(b)
